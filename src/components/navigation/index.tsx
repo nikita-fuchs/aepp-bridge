f2bc6d3f88eb429489698d3683798cc3e541f2ee
Yusuf Seyrek <seyrek91@gmail.com>
2024-04-19 18:02:54 +0300

    Add links to header

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,14 +1,34 @@
-import { Box, Toolbar, AppBar, Divider } from '@mui/material';
+import { Box, Toolbar, AppBar, Menu, Typography, MenuItem, Link, Button, Divider } from '@mui/material';
 
-import DarkLightSwitch from '../theme/DarkLightSwitch';
 import AeternityLogo from '../base/icons/logo';
+import ConnectWallet from './ConnectWallet';
+
+const linkStyles = { marginLeft: 1, textDecoration: 'none', ':hover': { textDecoration: 'underline' } };
 
 const NavigationBar = () => {
     return (
         <AppBar position="static" color="default" sx={{ boxShadow: 'none', background: '#ffffff' }}>
             <Toolbar>
-                <Box sx={{ flexGrow: 1, display: 'flex' }}>
+                <Box
+                    sx={{
+                        flexGrow: 1,
+                        display: 'flex',
+                        alignItems: 'center',
+                        flex: 1,
+                        justifyContent: 'space-between',
+                    }}
+                >
                     <AeternityLogo />
+                    <Box sx={{ alignItems: 'center', display: { sm: 'flex', xs: 'none' } }}>
+                        <Link sx={linkStyles} href="https://aeternity.com" target="_blank">
+                            æternity Website
+                        </Link>
+                        <Link sx={linkStyles} href="https://wallet.superhero.com" target="_blank">
+                            æternity Wallet
+                        </Link>
+                        <Divider flexItem orientation="vertical" sx={{ margin: 1, ml: 2 }} />
+                        <ConnectWallet />
+                    </Box>
                 </Box>
 
                 {/* <Divider flexItem orientation="vertical" sx={{ margin: 1 }} />
